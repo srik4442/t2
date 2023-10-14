35bea665ec601e9eaabaa2494cbbfc45f95fbf04
Srikara Thikkireddy <130203062+srik4442@users.noreply.github.com>
2023-10-14 22:32:50 +0530

    Update palette.js

--- src/components/palette.js
+++ src/components/palette.js
@@ -1,56 +1,65 @@
-import { Component } from "react";
-import "./style.css";
+import {Component}  from "react";
+import './style.css';
+export class Calculator extends Component{
+    constructor(){
+        super();
+        this.state={exp:"",history:""};
+    }
+    add=(event)=>{
+        this.setState({
+            exp:this.state.exp+event.target.id
+        });
+    }
+    calc=()=>{
+        try{
+            let m=eval(this.state.exp);
+            this.setState({
+                exp:m,
+                history:this.state.history+'\n'+this.state.exp
 
-export class Color extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      toggle: false,
+            });
+        }
+        catch(err){
+            this.setState({
+                exp:"Error"
+            });
+        }
+    }
+    clear =()=>{
+        this.setState({exp:""});
     };
-  }
-  changeColor=(event)=>{
-    const main1=document.getElementById("main1");
-    main1.style.backgroundColor=this.props[event.target.id];
-    console.log(this.props[event.target.id]);
-    this.setState({toggle:false});
-}
-
-handleClick=()=>{
-    this.setState(
-        {toggle:true}
-    );
-}
-
-  render() {
-    return (
-        <div id="main1">
-          <h1>Color Picker</h1>
-          {this.state.toggle ? (
-              <div id="main">
-                <div className="square" style= {{backgroundColor:this.props.color1}} id="color1" onClick={this.changeColor}></div>
-                <div className="square" style={{backgroundColor:this.props.color2}} id="color2"  onClick={this.changeColor}></div>
-                <div className="square" style={{backgroundColor:this.props.color3}}  id="color3"onClick={this.changeColor}></div>
-                <div className="square" style={{backgroundColor:this.props.color4}} id="color4"  onClick={this.changeColor}></div>
-                <div className="square" style={{backgroundColor:this.props.color5}} id="color5" onClick={this.changeColor}></div>
-                <div className="square" style={{backgroundColor:this.props.color6}} id="color6" onClick={this.changeColor}></div>
-                <div className="square" style={{backgroundColor:this.props.color7}} id="color7" onClick={this.changeColor}></div>
-                <div className="square" style={{backgroundColor:this.props.color8}} id="color8" onClick={this.changeColor}></div>
-                <div className="square" style={{backgroundColor:this.props.color9}} id="color9" onClick={this.changeColor}></div>
-                <div className="square" style={{backgroundColor:this.props.color10}} id="color10" onClick={this.changeColor}></div>
-                <div className="square" style={{backgroundColor:this.props.color11}} id="color11" onClick={this.changeColor}></div>
-                <div className="square" style={{backgroundColor:this.props.color12}} id="color12" onClick={this.changeColor}></div>
-                <div className="square" style={{backgroundColor:this.props.color13}} id="color13" onClick={this.changeColor}></div>
-                <div className="square" style={{backgroundColor:this.props.color14}} id="color14" onClick={this.changeColor}></div>
-                <div className="square" style={{backgroundColor:this.props.color15}} id="color15" onClick={this.changeColor}></div>
-                <div className="square" style={{backgroundColor:this.props.color16}} id="color16" onClick={this.changeColor}></div>
-              </div>
-          
-
-            
-          ) :  <div></div>}
-
-          <button onClick={this.handleClick}>Show Color Pallete</button>
-        </div>
-    );
-  }
+    render(){
+        return(
+            <div id="container">
+            <div id="screen">
+                <div className="camera"></div>
+                <p className="history">{this.state.history}</p>
+              <div className="exp"> <h2>{this.state.exp}</h2></div>
+            </div>
+            <div id="keys">
+            <div id="left">
+           <div class="key" id="7" onClick={this.add} >7️⃣</div>
+           <div class="key" id="8" onClick={this.add} >8️⃣</div>
+           <div class="key" id="9" onClick={this.add} >9️⃣</div>
+           <div class="key" id="4" onClick={this.add} >4️⃣</div>
+           <div class="key" id="5" onClick={this.add} >5️⃣</div>
+           <div class="key" id="6" onClick={this.add} >6️⃣</div>
+           <div class="key" id="1" onClick={this.add} >1️⃣</div>
+           <div class="key" id="2" onClick={this.add} >2️⃣</div>
+           <div class="key" id="3" onClick={this.add} >3️⃣</div>
+           <div class="key" id="0" onClick={this.add} >0️⃣</div>
+           <div class="key" id="." onClick={this.add} >⊡</div>
+           <div class="key" id="calc" onClick={this.calc} >🟰</div>
+           </div>
+           <div id="right">
+           <div class="key" id="back" onClick={this.clear}>🔙</div>
+           <div class="key" id="/" onClick={this.add} >➗</div>
+           <div class="key" id="*" onClick={this.add} >✖</div>
+           <div class="key" id="-" onClick={this.add} >➖</div>
+           <div class="key" id="+" onClick={this.add} >➕</div>
+            </div>
+            </div>
+            </div>
+        )
+    }
 }
